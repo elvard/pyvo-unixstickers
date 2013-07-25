7fe5c8f87ede6229b85553be20b66d33f3e7e0b9
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2013-07-25 13:41:54 +0200

    Skript změněn z AWK do Perlu.

---
+++ checkout.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -n
+
+use strict;
+use vars qw($file %contacts %per_user);
+use feature qw(say);
+
+if ($file ne $ARGV) {
+    chop;
+    $contacts{$ARGV} = $_;
+    $file = $ARGV;
+    next
+}
+
+if (/^(\d+) (.*)$/) {
+    my $qty = $1;
+    my $product = $2;
+
+    $per_user{$file}{$product} = $qty
+}
+
+END {
+    my %checkout, my $num = 1;
+
+    while (my ($user, $products) = each %per_user) {
+        say "Objednávka #$num ($contacts{$user})";
+        say "----------------";
+        while (my ($product, $qty) = each $products) {
+            print $qty, "x $product\n";
+            $checkout{$product} += $qty
+        }
+
+        say;
+        $num++
+    }
+
+    say "Celkem";
+    say "-------";
+    while (my ($product, $qty) = each %checkout) {
+        print $qty, "x $product\n"
+    }
+}
